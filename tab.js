d7e35bc21bc0e5bcff7887351aed1a4addf03c42
akg1812 <66181923+akg1812@users.noreply.github.com>
2020-07-28 17:17:17 +0530

    Update tab.js

--- tab.js
+++ tab.js
@@ -1,22 +1,62 @@
-//  using querySelectorAll that allows to select everything that has a particular selector
- const tabs= document.querySelectorAll('[data-tab-target]')
- const tabContents= document.querySelectorAll('[data-tab-content]')
 
- //to get single tabs
- tabs.forEach(tab =>{
-     tab.addEventListener('click',()=>{
-        const target = document.querySelector(tab.dataset.tabTarget)
-        tabContents.forEach(tabContent =>{
-            tabContent.classList.remove('active')
-        }) 
-        //in order to make active class only visibe
-        tabs.forEach(tab=>{
-            tab.classList.remove('active')
-        })
-         tab.classList.add('active')
-        // making only the tab that we click on active 
-         target.classList.add('active')
+document.getElementById('getId').addEventListener('click',getId);
+function getId(){
+    const URL = "https://jsonplaceholder.typicode.com/users";
+    
+    const main = document.getElementById("main");
+    main.innerHTML = "<p>Loading...";
+    
+    fetch(URL)
+      .then((response) => response.json())
+      .then((people) => main.innerHTML = getListOfIds(people));
+    
+    const getListOfIds = (people) => {
+      const ids = people
+        .map((person) => `<li>${person.id}</li>`)
+        .join("\n");
+    
+      return `<ul>${ids}</ul>`;
+    };
+}
+    
 
-     })
- })
 
+document.getElementById('getName').addEventListener('click',getName);
+    function getName(){
+    const URL = "https://jsonplaceholder.typicode.com/users";
+    
+    const main = document.getElementById("main");
+    main.innerHTML = "<p>Loading...";
+    
+    fetch(URL)
+      .then((response) => response.json())
+      .then((people) => main.innerHTML = getListOfNames(people));
+    
+    const getListOfNames = (people) => {
+      const names = people
+        .map((person) => `<li>${person.name}</li>`)
+        .join("\n");
+    
+      return `<ul>${names}</ul>`;
+    };
+}
+
+document.getElementById('getEmail').addEventListener('click',getEmail);
+function getEmail(){
+    const URL = "https://jsonplaceholder.typicode.com/users";
+    
+    const main = document.getElementById("main");
+    main.innerHTML = "<p>Loading...";
+    
+    fetch(URL)
+      .then((response) => response.json())
+      .then((people) => main.innerHTML = getListOfEmails(people));
+    
+    const getListOfEmails = (people) => {
+      const ids = people
+        .map((person) => `<li>${person.email}</li>`)
+        .join("\n");
+    
+      return `<ul>${ids}</ul>`;
+    };
+}
