ffba19ab13ac69dc3952ec7376185d7e29e59230
akg1812 <66181923+akg1812@users.noreply.github.com>
2020-07-31 22:42:04 +0530

    Update tab.js

--- tab.js
+++ tab.js
@@ -1,62 +1,117 @@
-
-document.getElementById('getId').addEventListener('click',getId);
-function getId(){
-    const URL = "https://jsonplaceholder.typicode.com/users";
-    
-    const main = document.getElementById("main");
-    main.innerHTML = "<p>Loading...";
-    
-    fetch(URL)
-      .then((response) => response.json())
-      .then((people) => main.innerHTML = getListOfIds(people));
-    
-    const getListOfIds = (people) => {
-      const ids = people
-        .map((person) => `<li>${person.id}</li>`)
-        .join("\n");
-    
-      return `<ul>${ids}</ul>`;
-    };
+//deafult loading of page
+function codeAddress() {
+ alert("the page is loaded");
+ return title.textContent;
+ 
 }
-    
+window.onload = codeAddress;
+const loading=document.querySelector(".loading");
+
+let ul_index = document.createElement("ul");
+ul_index.classList.add("index");
+
+let ul_name = document.createElement("ul");
+ul_name.classList.add("names");
+
+let ul_email = document.createElement("ul");
+ul_email.classList.add("emails");
 
 
-document.getElementById('getName').addEventListener('click',getName);
-    function getName(){
-    const URL = "https://jsonplaceholder.typicode.com/users";
-    
-    const main = document.getElementById("main");
-    main.innerHTML = "<p>Loading...";
-    
-    fetch(URL)
+
+let b1=document.createElement("button");
+    b1.textContent="id";
+    root.appendChild(b1);
+    b1.addEventListener("click",getId=()=>{
+
+let main=null;
+    ul_index.textContent="";
+    main = document.getElementById("main");
+    ul_index.style.display="inline-block";
+fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((people) => main.innerHTML = getListOfNames(people));
-    
-    const getListOfNames = (people) => {
-      const names = people
-        .map((person) => `<li>${person.name}</li>`)
-        .join("\n");
-    
-      return `<ul>${names}</ul>`;
-    };
-}
+      .then((people) =>  getListOfIds(people))
+      .catch((err)=> alert(err));
+      
+const getListOfIds = (people) => {
+        let temp =null;
+        people.forEach(person => {
+            temp=document.createElement("li");
+            temp.textContent=`${person.id}`;
+            ul_index.appendChild(temp)
+        })
+        console.log(ul_index);
+        main.appendChild(ul_index);
+        loading.style.display="none";
+        alert("error");
+        //ul_index.style.display="none";
+        ul_name.style.display="none";
+        ul_email.style.display="none";
+        //b1.style.display="none";
+       // title.style.display="none";
+        
 
-document.getElementById('getEmail').addEventListener('click',getEmail);
-function getEmail(){
-    const URL = "https://jsonplaceholder.typicode.com/users";
-    
-    const main = document.getElementById("main");
-    main.innerHTML = "<p>Loading...";
-    
-    fetch(URL)
+};
+})
+
+
+
+let b2=document.createElement("button");
+      b2.textContent="Name";
+      root.appendChild(b2);
+      b2.addEventListener("click",getName=()=>{
+let main=null;  
+     ul_name.textContent="";
+     main = document.getElementById("main");
+     ul_name.style.display="inline-block";
+fetch("https://jsonplaceholder.typicode.com/users")
       .then((response) => response.json())
-      .then((people) => main.innerHTML = getListOfEmails(people));
-    
-    const getListOfEmails = (people) => {
-      const ids = people
-        .map((person) => `<li>${person.email}</li>`)
-        .join("\n");
-    
-      return `<ul>${ids}</ul>`;
-    };
-}
+      .then((people) => getListOfNames(people));
+
+const getListOfNames = (people) =>{
+        let temp =null;
+        people.forEach(person => {
+            temp=document.createElement("li");
+            temp.textContent=`${person.name}`;
+            ul_name.appendChild(temp);
+        })
+        console.log(ul_name);
+        main.appendChild(ul_name);
+       
+        ul_email.style.display = "none";
+        ul_index.style.display="none";
+        //b2.style.display="none";
+       
+       
+};
+})
+
+
+  let b3=document.createElement("button");
+        b3.textContent="Email";
+        root.appendChild(b3);
+        b3.addEventListener("click",getEmail=()=>{
+  let main=null;
+        ul_email.textContent="";
+        main = document.getElementById("main");
+        ul_email.style.display = "inline-block";
+  fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((people) => getListOfNames(people));
+
+  const getListOfNames = (people) =>{
+        let temp =null;
+        people.forEach(person => {
+            temp=document.createElement("li");
+            temp.textContent=`${person.email}`;
+            ul_email.appendChild(temp)
+        })
+        console.log(ul_email);
+        main.appendChild(ul_email);
+        
+         ul_name.style.display = "none";
+         ul_index.style.display="none";
+        //b3.style.display="none";
+      
+        
+};
+})
